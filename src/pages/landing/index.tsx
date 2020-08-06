0c9980189e82ce541beaf89f49f387b93f93ceab
Eric Kivel <ericvkivel@gmail.com>
2020-08-06 12:32:28 -0300

    routes - css (landing page - study page)

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 
 import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
 
 
 function Landing() {
@@ -21,10 +27,19 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a href="" className="study">
-                        <img src={studyIcon} alt=""/>
-                    </a>
+                    <Link to="/study" className="study">
+                        <img src={studyIcon} alt="Estudar"/>
+                        Estudar
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas"/>
+                        Dar aulas
+                    </Link>
                 </div>
+
+                <span className="total-conections">
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                </span>
             </div>
         </div>
     );
